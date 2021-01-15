9933e0564d3959d4642cb281a3aefd15bf7a1a31
nikitar69 <nikitar69@gmail.com>
2021-01-15 20:16:15 +0100

    adding earliest changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+const arrayItems = [
+  {
+    name: "item1",
+    desription: "description1"
+  },
+  {
+    name: "item2",
+    desription: "description2"
+  },
+  {
+    name: "item3",
+    desription: "description3"
+  },
+  {
+    name: "item4",
+    desription: "description4"
+  },
+
+]
+function ActiveLink() {
+  function handleClick() {
+    arrayItems.map(item => {
+      return
+    })
+  }
+    return (
+      <div>{arrayItems.item.name}</div>
+    )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="train-header">
+        <div>Hello World</div>
       </header>
+
+      <div className="train-app-dashboard">
+        <div className="layout-positioning">
+          <div className="sidebar-menu">
+            <ul className="sidebar-items">
+              {arrayItems.map(item => {
+                return(<li onClick={handleClick}>{item.name} </li>)
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="description-item-panel">
+
+          {arrayItems.map(item => {
+            return(
+              <div>{item.desription}</div>
+              )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,6 @@
+// import React from "react";
+//
+// {arrayItems.map(item => {
+//   return(<li>{item.name}:{item.desription} </li>)
+// })}
+
